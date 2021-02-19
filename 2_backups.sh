b3c34cfa0bf90051033453c02fefe009f033d021
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-02-19 23:02:34 +0700

    Update 2_backups.sh

--- 2_backups.sh
+++ 2_backups.sh
@@ -10,10 +10,10 @@ if [ ! -d "${general_backups_dir}" ]; then mkdir -p "${general_backups_dir}"; fi
 if [ ! -d "${specific_backups_dir}" ]; then mkdir -p "${specific_backups_dir}"; fi
 
 ## Create specific backups (some files have glob patterns outside quote marks): ##
-cp "${domain_dir}/LocalSettings.php" "${specific_backups_dir}"
-cp "${domain_dir}/robots.txt" "${specific_backups_dir}"
-cp "${domain_dir}/${domain}.png" "${specific_backups_dir}"
-cp "${domain_dir}"/.htaccess* "${specific_backups_dir}"
+cp "${domain_dir}/LocalSettings.php" "${specific_backups_dir}" &&
+cp "${domain_dir}/robots.txt" "${specific_backups_dir}" &&
+cp "${domain_dir}/${domain}.png" "${specific_backups_dir}" &&
+cp "${domain_dir}"/.htaccess* "${specific_backups_dir}" &&
 cp "${domain_dir}"/google*.html "${specific_backups_dir}"
 
 ## Test specific backups: ##
