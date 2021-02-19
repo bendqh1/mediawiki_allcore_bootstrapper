c8154707dbc5ce5fa30e0661d92d282341514537
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-02-19 22:20:06 +0700

    Update 1_preface.sh

--- 1_preface.sh
+++ 1_preface.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# https://unix.stackexchange.com/questions/635287/xor-in-shell-globs
 # Shell glob patterns aren't expanded when quoted (unless in GNU find), so I didn't quote them;
 # With GNU find I used single quote marks instead double ones (with the exception of "${domain}" variable expansion and possibly its further path);
 
@@ -7,7 +8,7 @@
 set -x && complete -r
 # set -euo # If you plan to test the script line by line (or block by block) don't use it, as mistakes would terminate the session;
 
-## Declare web application root variables: ##
-web_application_root="${HOME}/public_html"
+## Declare web application root variables:
+web_application_root="${HOME}/public_html" 
 read domain
 domain_dir="${web_application_root}/${domain}"
