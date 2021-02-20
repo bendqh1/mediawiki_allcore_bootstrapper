a160e8fe9459234ffc482f8c06e3c079baead994
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-02-20 22:09:14 +0700

    Update 0.2_backups_preface.sh

--- 0.2_backups_preface.sh
+++ 0.2_backups_preface.sh
@@ -5,7 +5,6 @@ current_date="$(date +%d-%m-%Y-%H-%M-%S)" &&
 general_backups_dir="${HOME}/mediawiki_general_backups" &&
 specific_backups_dir="${HOME}/mediawiki_specific_backups"
 
-
 ## If backup directories don't exist, create them:
 rm -rf "${general_backups_dir}" &&
 mkdir -p "${general_backups_dir}" &&
