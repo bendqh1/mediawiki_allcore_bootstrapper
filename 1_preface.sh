f45a83fb99ebc8b1620adc2d3062b1fa24b2d6b5
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-02-20 13:45:26 +0700

    Update 1_preface.sh

--- 1_preface.sh
+++ 1_preface.sh
@@ -6,7 +6,8 @@
 # Set debug mode (if not dont already, you might want to add set -x && complete -r into .bashrc
 # set -euo # If you plan to test the script line by line (or block by block) don't use it, as mistakes would terminate the session;
 
-## Declare web application root variables:
+## Declare general variables:
 read domain &&
 web_application_root="${HOME}/www" &&
-domain_dir="${web_application_root}/${domain}/public_html"
+domain_dir="${web_application_root}/${domain}/public_html" &&
+current_date="$(date +%d-%m-%Y-%H-%M-%S)"
