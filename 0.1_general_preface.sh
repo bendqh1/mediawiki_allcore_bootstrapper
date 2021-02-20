96f23b02c6ecd3dd194cad8ab76e57895bacba2c
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-02-20 14:42:40 +0700

    Update and rename 0_preface.sh to 0.1_general_preface.sh

---
+++ 0.1_general_preface.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+## Declare latest mediawiki core download link:
+latest_mediawiki_core="https://releases.wikimedia.org/mediawiki/1.35/mediawiki-1.35.1.tar.gz"
+
+## Declare web application variables:
+read domain
+read db_name
+read db_nonroot_user_name
+web_application_root="${HOME}/www"
+domain_dir="${web_application_root}/${domain}/public_html"
