e9060906f1f76aca0f50e6a7a7ec2bc2eb235c16
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-02-20 14:23:19 +0700

    Update 0_preface.sh

--- 0_preface.sh
+++ 0_preface.sh
@@ -13,6 +13,6 @@ current_date="$(date +%d-%m-%Y-%H-%M-%S)" &&
 specific_backups_dir="${HOME}/mediawiki_specific_backups" &&
 general_backups_dir="${HOME}/mediawiki_general_backups"
 
-## If backup directories did not exist before, create them:
+## If backup directories don't exist, create them:
 if [ ! -d "${general_backups_dir}" ]; then mkdir -p "${general_backups_dir}"; fi &&
 if [ ! -d "${specific_backups_dir}" ]; then mkdir -p "${specific_backups_dir}"; fi
