17609aaa484c984051cfe227d0da8fabe6399291
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-02-20 14:28:55 +0700

    Update 1_database_backups.sh

--- 1_database_backups.sh
+++ 1_database_backups.sh
@@ -2,10 +2,11 @@
 
 ## Create database general backup:
 mysqldump -u"$db_nonroot_user_name" -p "$db_name" > "${general_backups_dir}/${db_nonroot_user_name}-${current_date}.sql"
-# -u && -p and db_name variable expansions shouldn't have "${}", rather, just "$";
-# If one wants to prompt db_nonroot_user_name password, -p should include a following whitespace character;
-# In general, db_nonroot_user_name and db_name variables must be different variables even if their values are the same;
-# Database Management Programs (i.e mysqldump, PHPMyAdmin) can produce databases in different sizes due to different data organization methods (as well as caching);
 
 ## Test database general backup:
 ll $general_backups_dir
+
+## Chapter notes
+# -u && -p and db_name variable expansions shouldn't be wrapped with "${}", rather, just with "$" (as above);
+# To directly prompt for db_nonroot_user_name PASSWORD, -p should include a following whitespace character (as above);
+# In general, db_nonroot_user_name and db_name variables must be different variables even if their values are the same (as above);
