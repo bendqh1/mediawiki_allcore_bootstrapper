7d9695f89a6e62809037fe4de94affeb1921acfd
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-02-20 21:47:27 +0700

    Update 0.2_backups_preface.sh

--- 0.2_backups_preface.sh
+++ 0.2_backups_preface.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 ## Declare existing installation backup variables:
-current_date="$(date +%d-%m-%Y-%H-%M-%S)"
-general_backups_dir="${HOME}/mediawiki_general_backups"
+current_date="$(date +%d-%m-%Y-%H-%M-%S)" &&
+general_backups_dir="${HOME}/mediawiki_general_backups" &&
 specific_backups_dir="${HOME}/mediawiki_specific_backups"
 
 
 ## If backup directories don't exist, create them:
-rm -rf "${general_backups_dir}"
-mkdir -p "${general_backups_dir}"
-rm -rf "${specific_backups_dir}"
+rm -rf "${general_backups_dir}" &&
+mkdir -p "${general_backups_dir}" &&
+rm -rf "${specific_backups_dir}" &&
 mkdir -p "${specific_backups_dir}"
+
+## Test existence of empty directories
+ll "${general_backups_dir}" &&
+ll "${specific_backups_dir}"
