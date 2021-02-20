36bd0d9602f1a1de4b92dc14c1f8e7f7cb4f8bd3
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-02-20 14:02:49 +0700

    Update 0_preface.sh

--- 0_preface.sh
+++ 0_preface.sh
@@ -6,18 +6,18 @@
 # Set debug mode (if not dont already, you might want to add set -x && complete -r into .bashrc
 # set -euo # If you plan to test the script line by line (or block by block) don't use it, as mistakes would terminate the session;
 
-## Declare general variables:
+## Declare web application variables:
 read domain &&
+read db_name &&
+read db_nonroot_user_name &&
 web_application_root="${HOME}/www" &&
 domain_dir="${web_application_root}/${domain}/public_html" &&
 
-## Declare general backup variables:
-current_date="$(date +%d-%m-%Y-%H-%M-%S)"
-
-## Declare database backup variables:
-read -s db_name &&
-read -s db_nonroot_user_name
-
-## Declare file tree backup variables:
+## Declare backup variables:
+current_date="$(date +%d-%m-%Y-%H-%M-%S)" &&
 specific_backups_dir="${HOME}/mediawiki_specific_backups" &&
 general_backups_dir="${HOME}/mediawiki_general_backups"
+
+## If backup directories did not exist before, create them:
+if [ ! -d "${general_backups_dir}" ]; then mkdir -p "${general_backups_dir}"; fi &&
+if [ ! -d "${specific_backups_dir}" ]; then mkdir -p "${specific_backups_dir}"; fi
