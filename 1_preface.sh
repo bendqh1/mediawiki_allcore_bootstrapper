ddebc30e970a7d27064e72b2c111d7c160e27f6c
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-02-20 13:29:35 +0700

    Update 1_preface.sh

--- 1_preface.sh
+++ 1_preface.sh
@@ -3,12 +3,10 @@
 # https://unix.stackexchange.com/questions/635287/xor-in-shell-globs
 # Shell glob patterns aren't expanded when quoted (unless in GNU find), so I didn't quote them;
 # With GNU find I used single quote marks instead double ones (with the exception of "${domain}" variable expansion and possibly its further path);
-
-## Set debug mode: ##
-set -x && complete -r
+# Set debug mode (if not dont already, you might want to add set -x && complete -r into .bashrc
 # set -euo # If you plan to test the script line by line (or block by block) don't use it, as mistakes would terminate the session;
 
 ## Declare web application root variables:
-read domain
-web_application_root="${HOME}/www"
+read domain &&
+web_application_root="${HOME}/www" &&
 domain_dir="${web_application_root}/${domain}/public_html"
