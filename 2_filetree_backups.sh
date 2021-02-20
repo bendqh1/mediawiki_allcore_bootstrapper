05138a42cbba6d52b71fa6543cc956f21794fd5c
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-02-20 21:49:06 +0700

    Update 2_filetree_backups.sh

--- 2_filetree_backups.sh
+++ 2_filetree_backups.sh
@@ -3,7 +3,7 @@
 ## Create filetree general backups:
 zip -r "${general_backups_dir}/${domain}-directory-backup-${current_date}.zip" "${domain_dir}"
 
-## Create filetree specific backups (Some files have glob patterns outside quote marks):
+## Create filetree specific backups (Some files have shell glob patterns outside quote marks):
 cp "${domain_dir}/LocalSettings.php" "${specific_backups_dir}" &&
 cp "${domain_dir}/robots.txt" "${specific_backups_dir}" &&
 cp "${domain_dir}/${domain}.png" "${specific_backups_dir}" &&
