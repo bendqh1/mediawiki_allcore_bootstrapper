065164996dbb69de805ebdb0b258c5cbe854e859
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-02-22 15:04:26 +0700

    Update 0.2_backups_preface.sh

--- 0.2_backups_preface.sh
+++ 0.2_backups_preface.sh
@@ -6,9 +6,7 @@ general_backups_dir="${HOME}/mediawiki_general_backups" &&
 specific_backups_dir="${HOME}/mediawiki_specific_backups"
 
 ## If backup directories don't exist, create them:
-rm -rf "${general_backups_dir}" &&
 mkdir -p "${general_backups_dir}" &&
-rm -rf "${specific_backups_dir}" &&
 mkdir -p "${specific_backups_dir}"
 
 ## Test existence of empty directories
