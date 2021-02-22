6005a5ddcc29d0ecadffaceff90b2e04fd23b7de
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-02-22 16:47:16 +0700

    Update 0_general_preface.sh

--- 0_general_preface.sh
+++ 0_general_preface.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-## Declare latest mediawiki core download link:
-latest_mediawiki_core="https://releases.wikimedia.org/mediawiki/1.35/mediawiki-1.35.1.tar.gz"
-
 ## Declare web application variables:
 read domain
 read web_application_root # ${HOME}/www
