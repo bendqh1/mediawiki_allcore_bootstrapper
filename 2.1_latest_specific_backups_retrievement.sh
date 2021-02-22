f394f162c5c9d40a6a55ab455740ad1ee9350369
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-02-22 15:23:07 +0700

    Create 2.1_latest_specific_backups_retrievement.sh

---
+++ 2.1_latest_specific_backups_retrievement.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+## Retreive latest specific backups to the new installation:
+cp -a "${latest_specific_backups_dir}"/.htaccess "${domain_dir}" # Only .htaccess file;
+cp -a "${latest_specific_backups_dir}"/* "${domain_dir}" # All files besides .htaccess;
+
+## Test specific backups retrievement:
+ll ${domain_dir}
