6f272136856cc5e8492ed47b2626b7f7c51dd075
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-02-22 15:33:15 +0700

    Update 0_general_preface.sh

--- 0_general_preface.sh
+++ 0_general_preface.sh
@@ -7,5 +7,5 @@ latest_mediawiki_core="https://releases.wikimedia.org/mediawiki/1.35/mediawiki-1
 read domain
 read db_name
 read db_nonroot_user_name
-read web_application_root
+read web_application_root # ${HOME}/www for SiteGround customers
 domain_dir="${web_application_root}/${domain}/public_html" # public_html is added for SiteGround customers
