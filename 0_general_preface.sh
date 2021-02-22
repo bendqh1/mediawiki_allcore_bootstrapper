d642138142441351fe12af69b34f11b2380e26d7
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-02-22 16:40:06 +0700

    Update 0_general_preface.sh

--- 0_general_preface.sh
+++ 0_general_preface.sh
@@ -5,7 +5,5 @@ latest_mediawiki_core="https://releases.wikimedia.org/mediawiki/1.35/mediawiki-1
 
 ## Declare web application variables:
 read domain
-read db_name
-read db_nonroot_user_name
 read web_application_root # For example: "${HOME}/www
 read domain_dir # For example: "${web_application_root}"/${domain}"/public_html
