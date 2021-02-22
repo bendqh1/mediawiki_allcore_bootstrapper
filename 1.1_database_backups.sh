cc94cbe137d4c4670f7ff154a00a808fe8b22b99
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-02-22 16:41:11 +0700

    Update 1.1_database_backups.sh

--- 1.1_database_backups.sh
+++ 1.1_database_backups.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+## Declare database variables:
+read db_name
+read db_nonroot_user_name
+
 ## Create database general backup:
 mysqldump -u"$db_nonroot_user_name" -p "$db_name" > "${latest_general_backups_dir}/${db_name}.sql"
 
 ## Test database general backup:
 ll $latest_general_backups_dir
 
-## Chapter notes
+## Chapter notes:
 # -u && -p and db_name variable expansions shouldn't be wrapped with "${}", rather, just with "$" (as above);
 # To directly prompt for db_nonroot_user_name PASSWORD, -p should include a following whitespace character (as above);
 # In general, db_nonroot_user_name and db_name variables must be different variables even if their values are the same (as above);
