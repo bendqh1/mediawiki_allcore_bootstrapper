30851205b6f62287022d4982770f0d06223a8cf2
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-02-22 19:13:30 +0700

    Update 0_general_preface.sh

--- 0_general_preface.sh
+++ 0_general_preface.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 ## Declare web application variables:
-read domain
 read web_application_root # ${HOME}/www
+read domain # example.com
 read domain_dir # ${web_application_root}/${domain}/public_html
