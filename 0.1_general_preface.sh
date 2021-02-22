408425281d4a6a539876bb4ee99cefcc1bf50639
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-02-22 15:02:16 +0700

    Update 0.1_general_preface.sh

--- 0.1_general_preface.sh
+++ 0.1_general_preface.sh
@@ -7,5 +7,5 @@ latest_mediawiki_core="https://releases.wikimedia.org/mediawiki/1.35/mediawiki-1
 read domain
 read db_name
 read db_nonroot_user_name
-web_application_root="${HOME}/www"
+read web_application_root
 domain_dir="${web_application_root}/${domain}"
