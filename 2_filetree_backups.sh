b02cf2d879e05715bf4342e49c8d85e5a910e9ae
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-02-22 15:13:23 +0700

    Update 2_filetree_backups.sh

--- 2_filetree_backups.sh
+++ 2_filetree_backups.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 ## Create filetree general backups:
-zip -r "${general_backups_dir}/${domain}-directory-backup-${current_date}.zip" "${domain_dir}"
+zip -r "${latest_general_backups_dir}/${domain}-directory-backup.zip" "${domain_dir}"
 
 ## Create filetree specific backups (Some files have shell glob patterns outside quote marks):
-cp "${domain_dir}/LocalSettings.php" "${specific_backups_dir}" &&
-cp "${domain_dir}/robots.txt" "${specific_backups_dir}" &&
-cp "${domain_dir}/${domain}.png" "${specific_backups_dir}" &&
-cp "${domain_dir}"/.htaccess* "${specific_backups_dir}" &&
-cp "${domain_dir}"/google*.html "${specific_backups_dir}"
+cp "${domain_dir}/LocalSettings.php" "${latest_specific_backups_dir}" &&
+cp "${domain_dir}/robots.txt" "${latest_specific_backups_dir}" &&
+cp "${domain_dir}/${domain}.png" "${latest_specific_backups_dir}" &&
+cp "${domain_dir}"/.htaccess* "${latest_specific_backups_dir}" &&
+cp "${domain_dir}"/google*.html "${latest_specific_backups_dir}"
 
 ## Test filetree general backups:
-ll $general_backups_dir
+ll $latest_general_backups_dir
 
 ## Test filetree specific backups:
-ll $specific_backups_dir
+ll $latest_specific_backups_dir
