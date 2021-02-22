b057d08956fb322ba911da92976da05384b24732
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-02-23 00:08:14 +0700

    Update and rename 2.0_installation.sh to 2.0_installation_preface.sh

---
+++ 2.0_installation_preface.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+## Declare latest mediawiki core download link:
+latest_mediawiki_core="https://releases.wikimedia.org/mediawiki/1.35/mediawiki-1.35.1.tar.gz"
+
+## Prepare to install:
+cd "${web_application_root}"
