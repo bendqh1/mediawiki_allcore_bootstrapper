354f1b342606a67827ba6ea286b8f23bfb9b5273
bendqh1 <79080969+bendqh1@users.noreply.github.com>
2021-02-22 23:53:22 +0700

    Update 1.0_backups_preface.sh

--- 1.0_backups_preface.sh
+++ 1.0_backups_preface.sh
@@ -5,10 +5,10 @@ current_date="$(date +%d-%m-%Y-%H-%M-%S)" &&
 latest_general_backups_dir="${HOME}/mediawiki_general_backups-${current_date}" &&
 latest_specific_backups_dir="${HOME}/mediawiki_specific_backups-${current_date}"
 
-## If backup directories don't exist, create them:
+## Create lateset backup directories:
 mkdir -p "${latest_general_backups_dir}" &&
 mkdir -p "${latest_specific_backups_dir}"
 
-## Test existence of empty directories
+## Test existence of empty latest backup directories
 ll "${latest_general_backups_dir}" &&
 ll "${latest_specific_backups_dir}"
